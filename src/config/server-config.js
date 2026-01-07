c627c773259aff46784245d0b26c5d1a6727ea5d
sourabh kumar <lordgk02@gmail.com>
2026-01-07 23:24:55 +0530

    added reverse proxy for servers

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
+    FLIGHT_SERVICE:process.env.FLIGHT_SERVICE,
+    BOOKING_SERVICE:process.env.BOOKING_SERVICE,
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     JWT_KEY:process.env.JWT_KEY,
     JWT_EXPIRY:process.env.JWT_EXPIRY
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const serverConfig = require('./config/server-config');
 const app = express();
 const limiter = rateLimit({
     windowMs: 2* 60* 1000,
-    max:5,
+    max:500,
 
 })
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(limiter)
+app.use('/flightservice',createProxyMiddleware({
+    target:serverConfig.FLIGHT_SERVICE,
+    changeOrigin:true,
+    pathRewrite:{'^/flightservice':'/'}
+}))
+app.use('/bookingservice',createProxyMiddleware({
+    target:serverConfig.BOOKING_SERVICE,
+    changeOrigin:true,
+    pathRewrite:{'^/bookingservice':'/'}
+}))
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
